3af631ec456c5372f41cdf46d08b0f204fe6e116
br3mah <nyeleti.bremah@gmail.com>
2022-03-15 09:18:38 +0200

    On Pause

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,7 +17,6 @@
    <p>LOADING</p>
   <div class="circ-one"></div><div class="circ-two"></div>
 </div>
-
     <div class="loader-section section-left"></div>
     <div class="loader-section section-right"></div>
 
